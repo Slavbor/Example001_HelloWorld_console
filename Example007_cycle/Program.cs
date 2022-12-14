ebc9c9914e48fba4e3083cf995cabdf5769ed210
Slavbor <shipmate@mail.ru>
2022-12-14 23:04:40 +0300

    Triangle size enlarged

--- Example007_cycle/Program.cs
+++ Example007_cycle/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("*");
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 27,
-    xc = 80, yc = 27;
+int xa = 50, ya = 1,
+    xb = 1, yb = 30,
+    xc = 100, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("*");
@@ -19,7 +19,7 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 8000)
+while (count < 30000)
 {
     int what = new Random().Next(0, 3); //[0;3) 0 1 2
     if(what == 0)
@@ -34,7 +34,7 @@ while (count < 8000)
         y = (y + yb) / 2;
     }
 
-    if(what == 2)
+    if(what == 2)  
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
