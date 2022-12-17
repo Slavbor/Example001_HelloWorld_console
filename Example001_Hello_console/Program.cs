40acd8920dbff2ded41da74a0c46cf5c89971233
Slavbor <shipmate@mail.ru>
2022-12-17 22:39:02 +0300

    add Example 8 & 9

--- Example001_Hello_console/Program.cs
+++ Example001_Hello_console/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("2 Hello, World!");
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {        
+        Console.WriteLine("Введите значение радиуса круга");
+        double r = Convert.ToDouble(Console.ReadLine()); // преобразуем введенную строку в число 
+                                                         // и присваиваем его значение переменной
+
+        double pi = Math.PI; //  число пи;
+
+        //формулы для вычислений можно найти в интернете
+
+        double perimeter = 2 * pi * r; //Запишите тут код для вычисления периметра круга
+        double area = pi * Math.Pow(r, 2); //Запишите тут код для вычисления площади круга
+
+        Console.WriteLine($"Периметр: {perimeter}, площадь: {area}"); 
+    }
+}
--- Example007_cycle/Program.cs
+++ Example007_cycle/Program.cs
@@ -1,10 +1,14 @@
 ﻿Console.Clear();
+
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("*");
 
-int xa = 50, ya = 1,
-    xb = 1, yb = 30,
-    xc = 100, yc = 30;
+int xa = 50,
+    ya = 1,
+    xb = 1,
+    yb = 30,
+    xc = 100,
+    yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("*");
@@ -15,26 +19,27 @@ Console.WriteLine("*");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("*");
 
-int x = xa, y = xb;
+int x = xa,
+    y = xb;
 
 int count = 0;
 
-while (count < 30000)
+while (count < 15000)
 {
-    int what = new Random().Next(0, 3); //[0;3) 0 1 2
-    if(what == 0)
+    int what = new Random().Next(0, 3); //[0;3] 0 1 2
+    if (what == 0)
     {
         x = (x + xa) / 2;
-        y = (y + ya) / 2;
+        y = (y + ya) / 2; 
     }
 
-    if(what == 1)
+    if (what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
 
-    if(what == 2)  
+    if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
@@ -42,6 +47,5 @@ while (count < 30000)
 
     Console.SetCursorPosition(x, y);
     Console.WriteLine("*");
-    count ++;
-
-}   
+    count++;
+}
