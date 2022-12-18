f6cb88e4e5f537cc113f2b121f5df90141efb12a
Slavbor <shipmate@mail.ru>
2022-12-18 22:42:11 +0300

    lection 2 completed

---
+++ Exmple010_array_Find/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+Write("Введите индекс массива для поиска: ");
+int find = Convert.ToInt32(ReadLine()!);
+
+int[] array = { 8, 23, 5, 1, 2, 3, 4 }; // length = 7
+
+int n = array.Length;
+
+int index = 0;
+
+while (index < n)
+{
+    if(index == find)
+    {
+        WriteLine("Под индексом массива: " + find + ", находится число: " + array[index]);
+    }
+        
+    index ++;
+}
